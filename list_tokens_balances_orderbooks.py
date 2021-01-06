bc61564ec483478ba09831ed2d25e4517c7800a9
tryiou <whtzfck@gmail.com>
2021-01-06 11:57:41 +0100

    add preferred_token2 priority and buffer to dx_get_my_markets(...)

--- list_tokens_balances_orderbooks.py
+++ list_tokens_balances_orderbooks.py
@@ -4,15 +4,15 @@ from utils import dxbottools
 class Market:
     MarketCount = 0
 
-    def __init__(self, pair, ask, bid):
+    def __init__(self, pair, m_ask=[], m_bid=[]):
         self.pair = pair
-        self.ask = ask
-        self.bid = bid
-        Market.MarketCount += 1
+        self.ask = m_ask
+        self.bid = m_bid
+        if self.ask or self.bid:
+            Market.MarketCount += 1
 
 
 # FUNCTIONS>>
-
 def dx_get_tokens_balance():
     balances = dxbottools.rpc_connection.dxGetTokenBalances()
     print("\nXbridge coins & balances:")
@@ -27,26 +27,38 @@ def dx_get_tokens_balance():
 
 
 def dx_get_my_markets(tokens_balances):
+    preferred_token2 = "LTC"  # Prioritize this token as coin2 if possible
     # Create a list of Market objects
-    markets = []
-    print("Listing markets and retrieving orderbook:")
+    buffer_markets = []
+    final_markets = []
+    print("Listing DX markets and retrieving orderbook:")
     # WE LOOP ALL OUR TOKENS TO PRODUCE TOKEN1
     for token1 in sorted(tokens_balances.keys()):
         # WE LOOP ALL OUR TOKENS AGAIN IN REVERSE MODE TO PRODUCE TOKEN2
-        for token2 in reversed(sorted(tokens_balances.keys())):
-            if token1 == token2:  # TESTED EVERY PAIR WITH TOKEN1/TOKEN2
-                break
-            elif token1 != "Wallet" and token2 != "Wallet":
-                ask, bid = dxbottools.getorderbook(token1, token2)
-                # ask[::-1] to reverse
-                print(token1, token2, ":")
-                if ask or bid:
-                    print("Got orderbook")
-                    markets.append(Market(token1 + "/" + token2, ask, bid))  # STORE DATA IN LIST
-                else:
-                    print("Orderbook empty")
+        for token2 in sorted(tokens_balances.keys()):
+            if token1 != token2:
+                # TEST IF PAIR(OR ITS REVERSE) IS ALREADY PRESENT IN OUR BUFFER LIST
+                pairing_exist1 = any(x for x in buffer_markets if x.pair == token1 + "/" + token2)
+                pairing_exist2 = any(x for x in buffer_markets if x.pair == token2 + "/" + token1)
+                buffer_markets.append(Market(token1 + "/" + token2))
+                if not (pairing_exist1 or pairing_exist2):
+                    if token1 != "Wallet" and token2 != "Wallet":
+                        if token1 == preferred_token2:  # REVERSE THE PAIR IF NEEDED TO SELECT PREFERRED TOKEN2
+                            t1 = token2
+                            t2 = token1
+                        else:
+                            t1 = token1
+                            t2 = token2
+                        f_ask, f_bid = dxbottools.getorderbook(t1, t2)
+                        print(t1 + "/" + t2, ":")
+                        if f_ask or f_bid:
+                            # STORE DATA IN LIST
+                            print("Got orderbook")
+                            final_markets.append(Market(t1 + "/" + t2, f_ask, f_bid))
+                        else:
+                            print("Orderbook empty")
     print("")
-    return markets  # List of Market objects
+    return final_markets  # List of Market objects
 
 
 # FUNCTIONS<<
@@ -62,13 +74,22 @@ my_markets = dx_get_my_markets(my_tokens_balances)
 my_markets.sort(key=lambda x: x.pair)  # Sort object list by object.pair
 for market in my_markets:
     # DISPLAY FIRST ORDERS AROUND CENTER PRICE ASK/BID
+
     print(market.pair, "FIRST SELL ORDER:")
-    print("PRICE:", market.ask[-1][0], "QUANTITY:", market.ask[-1][1], "ID:", market.ask[-1][2])
+    if market.ask:
+        print("PRICE:", market.ask[-1][0], "QUANTITY:", market.ask[-1][1], "ID:", market.ask[-1][2])
+    else:
+        print("NONE")
+
     print(market.pair, "FIRST BUY ORDER:")
-    print("PRICE:", market.bid[0][0], "QUANTITY:", market.bid[0][1], "ID:", market.bid[0][2])
-    if float(market.ask[-1][0]) < float(market.bid[0][0]):
+    if market.bid:
+        print("PRICE:", market.bid[0][0], "QUANTITY:", market.bid[0][1], "ID:", market.bid[0][2])
+    else:
+        print("NONE")
+    if market.ask and market.bid and float(market.ask[-1][0]) < float(market.bid[0][0]):
         print("POSSIBLE ARBITRAGE PROFIT ON", market.pair)
     print("")
+
 print(Market.MarketCount, "active markets with available tokens")
 
 # MAIN<<

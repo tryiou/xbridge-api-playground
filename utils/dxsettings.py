af3f61f166d31ab8b9e518cd10f6ec698e9a423b
tryiou <73646876+tryiou@users.noreply.github.com>
2021-01-06 16:09:41 +0100

    Update dxsettings.py

--- utils/dxsettings.py
+++ utils/dxsettings.py
@@ -11,3 +11,9 @@ tradingaddress['DOGE'] = '_address_'
 rpcport = 41414
 rpcuser = 'rpcuser'
 rpcpassword = 'rpcpassword'
+
+infura_url = "https://mainnet.infura.io/v3/YOURKEY"
+
+ccxt_cex = "bittrex"
+ccxt_cex_api = "YOUR_API"
+ccxt_cex_secret = "YOUR_API_SECRET"
